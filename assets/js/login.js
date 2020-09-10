d6fe857f3ba4b1afcd863b1e4d1a590da035f901
haha <1006078856@qq.com>
2020-09-10 08:59:47 +0800

    注册页面

---
+++ assets/js/login.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.reg').on('click', function() {
+        $('.login').hide()
+        $('.reg-box').show()
+    })
+    $('.tologin').on('click', function() {
+        $('.login').show()
+        $('.reg-box').hide()
+    })
+})
